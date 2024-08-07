3f1f67ca1718c5ca250d91965324045e0e1ec6ce
kabeer <kabeerkwatra@gmail.com>
2024-08-07 14:57:37 +0530

    Added picked up section to rider dashboard

--- app/components/PickupRequests.tsx
+++ app/components/PickupRequests.tsx
@@ -18,10 +18,12 @@ export default function(){
     const Orders = orders?.orders
     if (Orders){
         const unacceptedOrders=Orders.filter((order:any)=>order.accepted_by_rider==false)
-        const acceptedOrders=Orders.filter((order:any)=>order.accepted_by_rider==true && order.delivered==false)
-        const deliveredOrders=Orders.filter((order:any)=>order.delivered==true)
+        const acceptedOrders=Orders.filter((order:any)=>order.accepted_by_rider==true && order.picked_up==false)
+        const pickedUpOrders=Orders.filter((order:any)=>order.picked_up==true && order.delivered==false)
+        
         let noUnacceptedOrders = true
         let noAcceptedOrders = true
+        let noPickedUpOrders = true
         return <div className="flex flex-col font-semibold ">
                 <div className="text-5xl text-center mb-5 text-red-600">
                     Pickup requests
@@ -57,13 +59,34 @@ export default function(){
                             <div>ORDER ID = {o.id}</div>
                             <div>{actualOrder.map(([key,value])=><div key={key}>{`${key} x ${value}`}</div>)}</div>
                             <div>  <LoadingButton onClick={ async ()=>{
+                                await updateOrder(o.id,"picked_up")
+                                mutate()
+                                }} className="mt-2 p-1 border-2 border-red-600 text-white bg-red-600 text-xs">
+                                    Mark as picked up</LoadingButton>
+                                </div>
+                            </div>
+                    })}
+                    {noAcceptedOrders?<div className="mb-5">(No orders)</div>:null}
+                </div>
+                <div className="text-5xl text-center mb-5 text-red-600">
+                    Picked Up
+                </div>
+                <div className="flex flex-col justify-center items-center">
+                    {pickedUpOrders.map((o:any)=>{
+                        const actualOrder=Object.entries(JSON.parse(o.items)).filter(([key,value])=>value!=0)
+                        if(actualOrder.length==0) {return null}
+                        noPickedUpOrders=false
+                        return <div className="mb-5 flex flex-col items-center" key={o.id}>
+                            <div>ORDER ID = {o.id}</div>
+                            <div>{actualOrder.map(([key,value])=><div key={key}>{`${key} x ${value}`}</div>)}</div>
+                            <div>  <LoadingButton onClick={ async ()=>{
                                 await updateOrder(o.id,"delivered")
                                 mutate()
                                 }} className="mt-2 p-1 border-2 border-red-600 text-white bg-red-600 text-xs">Mark as delivered</LoadingButton>
                                 </div>
                             </div>
                     })}
-                    {noAcceptedOrders?<div className="mb-5">(No orders)</div>:null}
+                    {noPickedUpOrders?<div className="mb-5">(No orders)</div>:null}
                 </div>
                 
             </div>
